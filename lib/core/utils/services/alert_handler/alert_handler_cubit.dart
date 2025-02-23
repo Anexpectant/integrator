5d0e84567c091a3d16eef2723062516c0ee3f5a2
anexpectant <anexpectant@anexpectant.ir>
2025-02-23 14:10:25 +0330

    INTEGRATOR-10 #feat: adding alert handler and failures

---
+++ lib/core/utils/services/alert_handler/alert_handler_cubit.dart
@@ -0,0 +1,25 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:integrator/core/utils/services/alert_handler/failures.dart';
+
+part 'alert_handler_state.dart';
+
+@singleton
+class AlertHandlerCubit extends Cubit<AlertHandlerState> {
+  AlertHandlerCubit() : super(FailureFreeState());
+
+  raise(Alert failure, {int duration = 1}) async {
+    emit(FailureOccurred(failure));
+    await Future.delayed(Duration(seconds: duration));
+    emit(FailureFreeState());
+  }
+
+  info(Alert message, {int duration = 1}) async {
+    emit(InfoState(message));
+    await Future.delayed(Duration(seconds: duration));
+    emit(FailureFreeState());
+  }
+}
